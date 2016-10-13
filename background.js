6f3c611d73fa33e116e791b8b3f5887f3516627d
Cheng Ly <chengsieuly@gmail.com>
2016-10-12 21:58:21 -0700

    fixed sorting bug for different subdomains

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
+// let data;
 const organizeTabsBtnEl = document.getElementById('organize-tab-btn');
 organizeTabsBtnEl.addEventListener('click', organizeTabs);
+
+const organizeTypeDropdownEl = document.getElementById('organize-type-dropdown');
+organizeTypeDropdownEl.addEventListener('change', () => {
+  const selectedOption = organizeTypeDropdownEl.selectedIndex;
+});
+
+chrome.browserAction.onClicked.addListener(()=>{
+  organizeTabs();
+});
+
+
 
 function organizeTabs() {
   let windowOptions = {populate: true, windowTypes: ['normal']};
@@ -14,6 +26,9 @@ function sortTabsToWindows(windows){
   let urls = getAllTabIds(windows);
 
   for(let url in urls){
+    if(url === "singles" && urls[url].length === 0){
+      continue;
+    }
     let firstTab = urls[url].shift();
     chrome.windows.create({tabId:firstTab}, (window) => {
       chrome.tabs.move(urls[url], {windowId : window.id, index : -1});
@@ -26,6 +41,9 @@ function getAllTabIds(windows){
   let urls = {singles:[]};
   windows.forEach(function(window){
     window.tabs.forEach(function(tab){
+      if(tab.url === "chrome://extensions/"){
+        return;
+      }
       if(!urls[getBaseDomain(tab.url)]){
         urls[getBaseDomain(tab.url)] = [];
       }
@@ -43,5 +61,8 @@ function getAllTabIds(windows){
 function getBaseDomain(url){
   // thanks to anubhava on Stack Overflow: http://stackoverflow.com/questions/25703360/regular-expression-extract-subdomain-domain
   let domainRegex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im;
-  return domainRegex.exec(url)[1];
+  let baseDomain = domainRegex.exec(url)[1];
+  let baseDomainArr = baseDomain.split('.');
+
+  return baseDomainArr[baseDomainArr.length - 2];
 }

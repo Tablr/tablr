ac9e09a2f14d3713a51ac4a7bf8216ab770452af
Cheng Ly <chengsieuly@gmail.com>
2016-10-13 13:44:19 -0700

    refactoring helper functions

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,6 @@
+const helpers = require('./shared/helpers.js');
+
+
 /*** Classes ***/
 class Tab {
   constructor(id, url) {
@@ -5,46 +8,6 @@ class Tab {
     this.url = url;
   }
 }
-
-/* HELPER FUNCTIONS */
-// Get base domain
-const getBaseDomain = url => {
-  // thanks to anubhava on Stack Overflow: http://stackoverflow.com/questions/25703360/regular-expression-extract-subdomain-domain
-  let domainRegex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im;
-  let baseDomain = domainRegex.exec(url)[1];
-  let baseDomainArr = baseDomain.split('.');
-
-  return baseDomainArr[baseDomainArr.length - 2];
-};
-
-// Gather all urls and separate by domain
-const getAllTabIds = windows => {
-  const urls = { singles: [] };
-
-  // Structuring our urls with base urls and all their associated tabs
-  windows.forEach(window => {
-    window.tabs.forEach(tab => {
-      // Get the base domain for the url
-      // Ex. 'http://meta.stackoverflow.com/test' --> 'stackoverflow'
-      const baseDomain = getBaseDomain(tab.url);
-
-      // Update our urls with base domains and all associated Tabs
-      if (!urls[baseDomain]) urls[baseDomain] = [];
-      urls[baseDomain].push(new Tab(tab.id, baseDomain));
-    });
-  });
-
-  // After updating our urls, we will have a bunch of single base domains
-  // By design, we will push all of these to a single key
-  for (let base in urls ) {
-    if (urls[base].length <= 1 && base !== 'singles') {
-      urls.singles.push(urls[base][0]);
-      delete urls[base];
-    }
-  }
-
-  return urls;
-};
 
 /**********************************************************************/
 // We need super wrapper that does the initial gather of our data
@@ -133,3 +96,8 @@ chrome.runtime.onMessage.addListener(() => {
     let urls = getAllTabIds(windows);
   }
 });
+
+
+
+// exported to helper.js
+//============================

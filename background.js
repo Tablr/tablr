efdea9588a4281bfb2f47feec6f19653a2da6c06
Cheng Ly <chengsieuly@gmail.com>
2016-10-12 20:16:12 -0700

    add functionality to sort tabs to windows by domain

--- background.js
+++ background.js
@@ -1,1 +1,37 @@
-console.log('hello world!');
+chrome.browserAction.onClicked.addListener(()=>{
+  let windowOptions = {populate: true, windowTypes: ['normal']};
+  chrome.windows.getAll(windowOptions, sortTabsToWindows);
+});
+
+function sortTabsToWindows(windows){
+  let urls = {singles:[]};
+  windows.forEach(function(window){
+    window.tabs.forEach(function(tab){
+      if(!urls[getBaseDomain(tab.url)]){
+        urls[getBaseDomain(tab.url)] = [];
+      }
+      urls[getBaseDomain(tab.url)].push(tab.id)
+    });
+  });
+
+  for( let url in urls ){
+    if(urls[url].length <= 1 && url !== "singles"){
+      urls["singles"].push(urls[url][0]);
+      delete urls[url];
+    }
+  }
+
+  for(let url in urls){
+    let firstTab = urls[url].shift();
+    chrome.windows.create({tabId:firstTab}, (window) => {
+      chrome.tabs.move(urls[url], {windowId : window.id, index : -1});
+    });
+  }
+}
+
+/* helper functions */
+function getBaseDomain(url){
+  // thanks to anubhava on Stack Overflow: http://stackoverflow.com/questions/25703360/regular-expression-extract-subdomain-domain
+  let domainRegex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im;
+  return domainRegex.exec(url)[1];
+}
